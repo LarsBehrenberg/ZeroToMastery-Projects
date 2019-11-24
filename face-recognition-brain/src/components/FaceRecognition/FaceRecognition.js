8c2598615adf8898647837926f909626745bfe74
Lars Behrenberg <32539217+LarsBehrenberg@users.noreply.github.com>
2019-11-24 21:58:19 +0900

    fixing missing alt attribute

--- face-recognition-brain/src/components/FaceRecognition/FaceRecognition.js
+++ face-recognition-brain/src/components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ import './face-recognition.css';
 const FaceRecognition = ({ imgSrc }) => {
     return (
         <div className='img-container' id="img-container">
-                <img id='face-image' src={imgSrc} />
+                <img id='face-image' src={imgSrc} alt=''/>
         </div>
     )
 }
